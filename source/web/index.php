71a07b05b654a3a92c2e89694f1dd84743bf079e
Benoit Delboé <benoit@lescanumeriques.fr>
2022-05-17 09:04:39 +0200

    install coralguardian theme

--- source/web/index.php
+++ source/web/index.php
@@ -2,6 +2,7 @@
 /**
  * WordPress View Bootstrapper
  */
+define("ICL_LANGUAGE_CODE", "fr");
 require_once __DIR__."/../vendor/autoload.php";
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
